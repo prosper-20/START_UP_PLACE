60a0987047130fb389b1c69d9cd0715cdf07da98
prosper <edwardprosper001@gmail.com>
2022-08-15 22:36:46 -0700

    updates

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
@@ -8,3 +9,17 @@ def home(request):
 
 
 def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Hi {username}, your account has been created successfully..Kindly login below")
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+    
+    context = {
+        "form": form
+    }   
+    return render(request, "users/")    
