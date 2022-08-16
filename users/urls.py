adf33e561c57d0eebf52ac419bee89602b31482b
prosper <edwardprosper001@gmail.com>
2022-08-15 22:30:23 -0700

    updates

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import home
-
+from django.contrib.auth import views
 
 urlpatterns = [
     path("", home, name="home"),
--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def home(request):
     return render(request, "users/home.html")
+
+
+def register(request):
