e7b33d4c89fb0b12ff1cf204f2332f174a0baa58
prosper <edwardprosper001@gmail.com>
2022-08-15 22:40:31 -0700

    updates

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, register
 from django.contrib.auth import views
 
 urlpatterns = [
     path("", home, name="home"),
+    path('register/', register, name="register")
 ]
